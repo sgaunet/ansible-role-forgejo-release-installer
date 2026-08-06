8fdc86876e04543349dcb055a68985cfcbe99e7e
Sylvain <1552102+sgaunet@users.noreply.github.com>
2026-08-06 20:12:41 +0200

    ci(workflows): run molecule on pull requests, drop monthly cron
    ci(workflows): run molecule on pull requests, drop monthly cron

--- gen-ci.sh
+++ gen-ci.sh
@@ -24,18 +24,4 @@ do
   else
     sed -i "s/BINNAME/${bin_name}/g" ".github/workflows/${bin_name}.yml"
   fi
-
-  RANDOM_DAY=$(echo $((1 + RANDOM % 5)))
-  RANDOM_HOUR=$(echo $((1 + RANDOM % 23)))
-  RANDOM_MIN=$(echo $((1 + RANDOM % 59)))
-
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' "s/RANDOM_DAY/${RANDOM_DAY}/g" ".github/workflows/${bin_name}.yml"
-    sed -i '' "s/RANDOM_HOUR/${RANDOM_HOUR}/g" ".github/workflows/${bin_name}.yml"
-    sed -i '' "s/RANDOM_MIN/${RANDOM_MIN}/g" ".github/workflows/${bin_name}.yml"
-  else
-    sed -i "s/RANDOM_DAY/${RANDOM_DAY}/g" ".github/workflows/${bin_name}.yml"
-    sed -i "s/RANDOM_HOUR/${RANDOM_HOUR}/g" ".github/workflows/${bin_name}.yml"
-    sed -i "s/RANDOM_MIN/${RANDOM_MIN}/g" ".github/workflows/${bin_name}.yml"
-  fi
 done
